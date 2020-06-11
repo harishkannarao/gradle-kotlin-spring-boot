a1f5aded12590eab8f45603ce0ad34108495c392
harishkannarao <harish.gtec@gmail.com>
2020-06-11 13:22:38 +0530

    Initial skeleton from spring initializer

---
+++ src/test/kotlin/com/harishkannarao/springboot/kotlin/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.harishkannarao.springboot.kotlin.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
