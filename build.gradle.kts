d0d2c42f311dbeaa8a0991493efd9086eff88277
harishkannarao <harish.gtec@gmail.com>
2020-06-11 16:01:56 +0530

    applying plugin explicitly

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot")
-	id("io.spring.dependency-management")
-	kotlin("jvm")
-	kotlin("plugin.spring")
+	id("java")
+	id("org.springframework.boot").apply(false)
+	id("io.spring.dependency-management").apply(false)
+	kotlin("jvm").apply(false)
+	kotlin("plugin.spring").apply(false)
 }
 
 // variables for gradle.properties
@@ -14,29 +15,38 @@ group = "com.harishkannarao.springboot.kotlin"
 version = ""
 java.sourceCompatibility = JavaVersion.toVersion(javaVersion)
 
-repositories {
-	mavenCentral()
-}
+allprojects {
 
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	apply(plugin= "java")
+	apply(plugin= "org.springframework.boot")
+	apply(plugin= "io.spring.dependency-management")
+	apply(plugin= "org.jetbrains.kotlin.jvm")
+	apply(plugin= "org.jetbrains.kotlin.plugin.spring")
+
+	repositories {
+		mavenCentral()
 	}
-}
 
-tasks.withType<Test> {
-	useJUnitPlatform()
-	val properties = System.getProperties().entries.map { it.key.toString() to it.value }.toMap()
-	systemProperties(properties)
-}
+	dependencies {
+		implementation("org.springframework.boot:spring-boot-starter-web")
+		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+		implementation("org.jetbrains.kotlin:kotlin-reflect")
+		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+		testImplementation("org.springframework.boot:spring-boot-starter-test") {
+			exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+		}
+	}
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = javaVersion
+	tasks.withType<Test> {
+		useJUnitPlatform()
+		val properties = System.getProperties().entries.map { it.key.toString() to it.value }.toMap()
+		systemProperties(properties)
+	}
+
+	tasks.withType<KotlinCompile> {
+		kotlinOptions {
+			freeCompilerArgs = listOf("-Xjsr305=strict")
+			jvmTarget = javaVersion
+		}
 	}
 }
