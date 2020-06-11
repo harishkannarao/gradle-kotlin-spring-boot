6b0ff573a41f936501d2af8f1b50478f8095bc6c
harishkannarao <harish.gtec@gmail.com>
2020-06-11 15:36:59 +0530

    Using jvm static companion object

--- src/main/kotlin/com/harishkannarao/springboot/kotlin/demo/DemoApplication.kt
+++ src/main/kotlin/com/harishkannarao/springboot/kotlin/demo/DemoApplication.kt
@@ -4,8 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DemoApplication
-
-fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+class DemoApplication {
+	companion object {
+		@JvmStatic
+		fun main(args: Array<String>) {
+			runApplication<DemoApplication>(*args)
+		}
+	}
 }
