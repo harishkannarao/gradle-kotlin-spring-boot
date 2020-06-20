aed5d2808244fd0944ea55d58e7061a36344d420
harishkannarao <harish.gtec@gmail.com>
2020-06-20 19:52:49 +0100

    Using coroutine scope for parallel loading

--- application/src/main/kotlin/com/harishkannarao/springboot/kotlin/demo/dao/CustomerRepository.kt
+++ application/src/main/kotlin/com/harishkannarao/springboot/kotlin/demo/dao/CustomerRepository.kt
@@ -1,6 +1,8 @@
 package com.harishkannarao.springboot.kotlin.demo.dao
 
 import com.harishkannarao.springboot.kotlin.demo.dto.CustomerResponseDto
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import org.springframework.stereotype.Component
@@ -15,18 +17,9 @@ class CustomerRepository {
         )
     }
 
-    suspend fun getAll(): Flow<CustomerResponseDto> {
-        return listOf(
-                CustomerResponseDto(
-                        id = "1",
-                        firstName = "firstName-1",
-                        lastName = "lastName-1"
-                ),
-                CustomerResponseDto(
-                        id = "2",
-                        firstName = "firstName-2",
-                        lastName = "lastName-2"
-                )
-        ).asFlow()
+    suspend fun getAll(): Flow<CustomerResponseDto> = coroutineScope {
+        val customer1 = async { getById("1") }
+        val customer2 = async { getById("2") }
+        listOf(customer1.await(), customer2.await()).asFlow()
     }
 }
