7193c1c12e205cd93fa531284b62a4f745581ec5
harishkannarao <harish.gtec@gmail.com>
2020-06-22 08:26:28 +0100

    Enabling Netty access logs

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -1,2 +1,8 @@
+import org.springframework.boot.gradle.tasks.run.BootRun
+
 apply(plugin= "org.springframework.boot")
 apply(plugin= "org.jetbrains.kotlin.plugin.spring")
+
+tasks.getByName<BootRun>("bootRun") {
+    systemProperty("reactor.netty.http.server.accessLogEnabled","true")
+}
