d0d9b7163b9a0d8c177abf339f9ee7e4e9da4936
harishkannarao <harish.gtec@gmail.com>
2020-06-26 19:30:54 +0100

    Removing GlobalScope

--- application/src/test/kotlin/com/harishkannarao/springboot/kotlin/demo/concurrency/DemoApplication.kt
+++ application/src/test/kotlin/com/harishkannarao/springboot/kotlin/demo/concurrency/DemoApplication.kt
@@ -9,26 +9,28 @@ class DemoApplication {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val autoIngestionService = AutoIngestionService(
-                    intervalInMillis = 1500L,
-                    bufferSize = 5
-            )
-            val autoConsumerService = AutoConsumerService(
-                    autoIngestionService = autoIngestionService,
-                    intervalInMillis = 500L
-            )
-            GlobalScope.launch { // launch a new coroutine in background and continue
-                autoIngestionService.start()
-            }
-            GlobalScope.launch { // launch a new coroutine in background and continue
-                autoConsumerService.start()
-            }
             runBlocking {
+                val autoIngestionService = AutoIngestionService(
+                        intervalInMillis = 1500L,
+                        bufferSize = 5
+                )
+                val autoConsumerService = AutoConsumerService(
+                        autoIngestionService = autoIngestionService,
+                        intervalInMillis = 500L
+                )
+                val ingestionJob = launch { // launch a new coroutine in background and continue
+                    autoIngestionService.start()
+                }
+                val consumerJob = launch { // launch a new coroutine in background and continue
+                    autoConsumerService.start()
+                }
                 while (true) {
                     val result = GlobalScope.async {
                         autoConsumerService.observe(5)
                     }
                     if (result.await()) {
+                        ingestionJob.cancel()
+                        consumerJob.cancel()
                         break
                     }
                 }
